394b1ac666108b1a402afb736e0ce44aba5924d4
Shishirthakur2416 <159231964+Shishirthakur2416@users.noreply.github.com>
2024-07-28 17:43:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+//  src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js">
+
+    var typed = new Typed('#element', {
+        strings: [' Front-end Web Developer.'],
+        typeSpeed: 80,
+    });
